eda3c7b39d6f36b6aec4b7a4a2654ba08a74bc12
islam test <arjb605@gmail.com>
2024-11-05 13:41:32 +0200

    working till here

--- lib/core/widgets/custom_data_list_view_item.dart
+++ lib/core/widgets/custom_data_list_view_item.dart
@@ -10,56 +10,59 @@ class CustomDataListViewItem extends StatelessWidget {
   final DataModel model;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 96,
-      width: 164,
-      decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(5),
-          boxShadow: [
-            BoxShadow(
-                color: AppColors.grey,
-                blurRadius: 8,
-                offset: const Offset(5, 5))
-          ]),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          const SizedBox(
-            width: 16,
-          ),
-          SizedBox(
-            height: 47,
-            width: 65,
-            child: Text(
-              model.name,
-              style: CustomTextStyles.poppins500style18
-                  .copyWith(fontSize: 16, color: AppColors.deepBrown),
-              textAlign: TextAlign.center,
-              overflow: TextOverflow.ellipsis,
-              maxLines: 3,
+    return GestureDetector(
+      onTap: () {},
+      child: Container(
+        height: 96,
+        width: 164,
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(5),
+            boxShadow: [
+              BoxShadow(
+                  color: AppColors.grey,
+                  blurRadius: 8,
+                  offset: const Offset(5, 5))
+            ]),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(
+              width: 16,
             ),
-          ),
-          SizedBox(
-            height: 64,
-            width: 47,
-            child: CachedNetworkImage(
-              imageUrl: model.image,
-              placeholder: (context, url) => Shimmer.fromColors(
-                  baseColor: AppColors.grey,
-                  highlightColor: AppColors.offWhite,
-                  child: Container(
-                    width: 47,
-                    height: 64,
-                    color: AppColors.grey,
-                  )),
-              errorWidget: (context, url, error) => const Icon(Icons.error),
+            SizedBox(
+              height: 47,
+              width: 65,
+              child: Text(
+                model.name,
+                style: CustomTextStyles.poppins500style18
+                    .copyWith(fontSize: 16, color: AppColors.deepBrown),
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                maxLines: 3,
+              ),
             ),
-          ),
-          const SizedBox(
-            width: 16,
-          ),
-        ],
+            SizedBox(
+              height: 64,
+              width: 47,
+              child: CachedNetworkImage(
+                imageUrl: model.image,
+                placeholder: (context, url) => Shimmer.fromColors(
+                    baseColor: AppColors.grey,
+                    highlightColor: AppColors.offWhite,
+                    child: Container(
+                      width: 47,
+                      height: 64,
+                      color: AppColors.grey,
+                    )),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              ),
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
